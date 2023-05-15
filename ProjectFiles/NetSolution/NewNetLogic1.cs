384a42c38061db04db92e5949368ea35ebf62972
AzuraZhang <Azura.Zhang@rockwellautomation.com>
2023-05-15 10:44:49 +0800

    add a new netlogic

---
+++ ProjectFiles/NetSolution/NewNetLogic1.cs
@@ -0,0 +1,25 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.UI;
+using FTOptix.Retentivity;
+using FTOptix.NativeUI;
+using FTOptix.Core;
+using FTOptix.CoreBase;
+using FTOptix.NetLogic;
+#endregion
+
+public class NewNetLogic1 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
